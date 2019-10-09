0d1dea19ad03812d8e04abe9c5c591760d76acb5
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2019-10-09 17:39:54 +0200

    Initial commit

---
+++ src/test/java/wawer/kamil/notetask/NotetaskApplicationTests.java
@@ -0,0 +1,16 @@
+package wawer.kamil.notetask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotetaskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
